32937fbfece4b6d5d7476e4b01bcd8096ee8d2db
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-03 17:02:44 +0100

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,8 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
-          <div className={styles.shape} />
+          {/* <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+          <div className={styles.shape} /> */}
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
