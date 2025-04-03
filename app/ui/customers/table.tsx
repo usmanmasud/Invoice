aa1bc940188fd325c7e24473d65a0863c999e670
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-03 17:51:33 +0100

    update fonts name

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { lusitana } from "../../ui/font";
+import { lusitana } from "../../ui/fonts";
 import Search from "@/app/ui/search";
 import {
   CustomersTableType,
